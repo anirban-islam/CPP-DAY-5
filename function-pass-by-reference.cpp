0502b9380acad1324fb592d19a17fc85382f7e94
unknown <anirbanislamemon.online@gmail.com>
2025-08-11 22:56:14 +0200

    Function-Pass-By-Reference-In-C++

---
+++ function-pass-by-reference.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+//You can also pass a reference to the function.
+//This can be useful when you need to change the value of the arguments:
+void swapNums(int &x, int &y) {
+  int z = x;
+  x = y;
+  y = z;
+}
+
+void modifyStr(string &str) {
+  str += " World!";
+}
+
+int main() {
+  int firstNum = 10;
+  int secondNum = 20;
+
+  cout << "Before swap: " << "\n";
+  cout << firstNum << secondNum << "\n";
+
+  // Call the function, which will change the values of firstNum and secondNum
+  swapNums(firstNum, secondNum);
+
+  cout << "After swap: " << "\n";
+  cout << firstNum << secondNum << "\n";
+
+
+
+  string greeting = "Hello";
+  modifyStr(greeting);
+  cout << greeting;
+
+  return 0;
+}
